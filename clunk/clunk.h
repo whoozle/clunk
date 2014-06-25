59f0d4f5d951dec7c9e81280512da7bc8ca7d503
Vladimir Menshakov <whoozle@yandex.ru>
2014-06-25 12:27:05 +0400

    added types.h include in clunk.h

--- clunk/clunk.h
+++ clunk/clunk.h
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
+#include <clunk/types.h>
 #include <clunk/v3.h>
 #include <clunk/context.h>
 #include <clunk/object.h>
