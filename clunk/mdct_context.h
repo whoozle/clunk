e868799065ee770dbf0e42c4bd78087778e9d8d6
Dmitry Marakasov <amdmi3@amdmi3.ru>
2014-10-08 18:21:26 +0400

    Explicitly initialize the const member
    Fixes build with clang

--- clunk/mdct_context.h
+++ clunk/mdct_context.h
@@ -61,7 +61,7 @@ public:
 
 	T data[N];
 
-	mdct_context() : sqrt_N((T)sqrt((T)N)), data() {
+	mdct_context() : window_func(), sqrt_N((T)sqrt((T)N)), data() {
 		for(unsigned t = 0; t < N4; ++t) {
 			angle_cache[t] = std::polar<T>(1, 2 * T(M_PI) * (t + T(0.125)) / N);
 		}
