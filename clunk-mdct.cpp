9aba97e68817f4dce04a5286413656dc339773e5
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-11-26 19:13:43 +0000

    added clunk-mdct tool stub

---
+++ clunk-mdct.cpp
@@ -0,0 +1,12 @@
+#include <clunk/mdct_context.h>
+#include <memory>
+
+int main(int argc, char **argv)
+{
+	if (argc < 0)
+	{
+		printf("usage: forward|inverse file.wav\n");
+		return 0;
+	}
+	return 0;
+}
