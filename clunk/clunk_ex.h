e0f4e684bed21122bed5a2be33e7525a2524f132
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-09-19 14:46:27 +0100

    reformatted exception a bit

--- clunk/clunk_ex.h
+++ clunk/clunk_ex.h
@@ -13,7 +13,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -24,24 +24,28 @@
 #include <string>
 #include <clunk/logger.h>
 
-namespace clunk {
-	class CLUNKAPI Exception : public std::exception {
-	public: 
-		Exception() throw() {} 
-		void add_message(const char *file, int line);
-		void add_message(const std::string &msg);
-		virtual void add_custom_message() {}
-		
-		virtual ~Exception() throw() {}
-		virtual const char* what() const throw() { return message.c_str(); }
-	private: 
-		std::string message;
-	};
-	
-	class CLUNKAPI IOException : public Exception {
-	public: 
-		virtual void add_custom_message();
-	};
+namespace clunk
+{
+
+    class CLUNKAPI Exception : public std::exception
+    {
+    public:
+        Exception() throw() {}
+        void add_message(const char *file, int line);
+        void add_message(const std::string &msg);
+        virtual void add_custom_message() {}
+
+        virtual ~Exception() throw() {}
+        virtual const char* what() const throw() { return message.c_str(); }
+    private:
+        std::string message;
+    };
+
+    class CLUNKAPI IOException : public Exception
+    {
+    public:
+        virtual void add_custom_message();
+    };
 }
 
 #define throw_generic(ex_cl, fmt) { ex_cl e; e.add_message(__FILE__, __LINE__); e.add_message(clunk::format_string fmt ); e.add_custom_message(); throw e; }
@@ -52,7 +56,7 @@ namespace clunk {
 #define CATCH(where, code) catch(const std::exception &_e) {\
 		LOG_ERROR(("%s: %s", where, _e.what())); \
 		code;\
-	} 
+	}
 
 #endif
 
