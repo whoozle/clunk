c36e1234f450952e74dddedd3a57d7b994b7c192
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-09-04 21:59:27 +0100

    reduce length by 1

--- clunk/clunk_ex.cpp
+++ clunk/clunk_ex.cpp
@@ -49,9 +49,9 @@ void clunk::IOException::add_custom_message() {
 	memset(buf, 0, sizeof(buf));
 
 #ifdef _MSC_VER
-	strncpy(buf, _strerror(NULL), sizeof(buf));
+	strncpy(buf, _strerror(NULL), sizeof(buf) - 1);
 #else 
-	strncpy(buf, strerror(errno), sizeof(buf));
+	strncpy(buf, strerror(errno), sizeof(buf) - 1);
 //	if (strerror_r(errno, buf, sizeof(buf)-1) != 0) perror("strerror");
 #endif
 	add_message(buf);
